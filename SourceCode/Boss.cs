eb07aedb26e95753b6085505b3ee784462062dde
Mohammed Hamarash <mohammed_hamarashi@hotmail.com>
2024-11-02 21:03:04 +0100

    Add Boss & Player Class
    Har addet en Boss og Player Class til koden. Boss DamagePlayer metoden mangler stadig, da jeg ikke kan lavet det helt færdig uden mat spørgsmålene er klar.

---
+++ SourceCode/Boss.cs
@@ -0,0 +1,25 @@
+﻿namespace DefaultNamespace;
+public class Boss
+{
+    //Attributes
+    public static int HealthPoints = 100;
+    
+    
+    //Method to attack Player HP, if Player gets answer wrong.
+    public void DamagePlayer()
+    {
+        //If the question is answered false
+        if (Question == false)
+        {
+            Player.HealthPoints -= 10;
+            Console.WriteLine($"Wrong Answer. You lose 10 HP. Your Current HP: {Player.HealthPoints}");
+        }
+        //If the question is answered right
+        else if (Question == true)
+        {
+            Boss.HealthPoints -= 10;   
+            Console.WriteLine($"CORRECT! Nice One. Boss Current HP: {Boss.HealthPoints}");
+        }
+    }
+    
+}
---
+++ SourceCode/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace DefaultNamespace;
+public class Player
+{
+    public string name;
+    public static int HealthPoints = 100;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+
+}
