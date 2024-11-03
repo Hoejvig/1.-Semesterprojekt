711f27dc5b8d2d7c9873dbe094da949f728313a9
MikkelHebel <mikkelhebelv2@gmail.com>
2024-11-03 15:38:40 +0100

    Added the math solve command

--- SourceCode/CommandGo.cs
+++ SourceCode/CommandGo.cs
@@ -8,7 +8,7 @@ class CommandGo : BaseCommand, ICommand {
   
   public void Execute (Context context, string command, string[] parameters) {
     if (GuardEq(parameters, 1)) {
-      Console.WriteLine("I don't seem to know where that is 🤔");
+      Console.WriteLine("I don't seem to know where that is. Correct usage: go <space>");
       return;
     }
     context.Transition(parameters[0]);
