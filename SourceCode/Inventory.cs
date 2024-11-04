fa6831de40b424e70a26c6384b8489791d93b525
Mohammed Hamarash <mohammed_hamarashi@hotmail.com>
2024-11-04 12:14:55 +0100

    Har ændret en smule i Inventory og Item
    HAr ændret en smule i Inventory og har slettet noget i item da det allerede ligger i inventory

---
+++ SourceCode/Inventory.cs
@@ -0,0 +1,32 @@
+namespace WorldOfMath;
+
+public class Inventory
+{
+    
+    // Array for storing badges
+    public Item[] items = new Item[5];
+    
+    // Method to show inventory
+    public void ShowInventory () 
+    {
+        Console.WriteLine("Inventory:");
+        foreach (Item item in items) 
+        {
+            Console.WriteLine(" - " + item);
+        }
+    }
+    
+    // Attribute to see where to add badges in array index
+    int NextOpenIndex = 0;
+    
+    
+    // Method to add badges after boss fight
+    public void AddItem(Item Item)
+    {
+        if (NextOpenIndex < items.Length)
+        {
+            items[NextOpenIndex] = Badge;
+        }
+        NextOpenIndex++;
+    }
+}
---
+++ SourceCode/Item.cs
@@ -0,0 +1,14 @@
+namespace WorldOfMath;
+
+
+public class Item
+{
+    //Attributes 
+    public string ItemName; 
+
+    //consructor
+    public Item(string ItemName)
+    {
+        this.ItemName = ItemName;
+    }
+}
